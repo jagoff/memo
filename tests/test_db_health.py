eaf0cae19b5ebcce739b3c3e0aa12607786d7f1d
Fernando Ferrari <fernandoferrari@gmail.com>
2026-06-14 22:01:38 -0300

    test: repoint inherited tests to refactored module locations (_db_health_report->cli_diag, _log_consult->server_common.log_consult)

--- tests/test_db_health.py
+++ tests/test_db_health.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import sqlite3
 from pathlib import Path
 
-from memo.cli import _db_health_report
+from memo.cli_diag import _db_health_report
 from memo.config import Config
 
 
